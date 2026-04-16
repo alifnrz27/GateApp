c428c6c03545ea47fdd7b295229567d476a22b4b
alifnrz27 <alifnaufalrizki27@gmail.com>
2026-04-16 20:41:24 +0700

    tes

--- backend/gpio/gpio.go
+++ backend/gpio/gpio.go
@@ -2,27 +2,29 @@
 package gpio
 
 import (
-	"fmt"
+	"time"
+
+	"github.com/stianeikeland/go-rpio/v4"
 )
 
-// func TriggerRelay(pinNumber int) error {
-// 	err := rpio.Open()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer rpio.Close()
-
-// 	pin := rpio.Pin(pinNumber)
-// 	pin.Output()
-
-// 	pin.High()
-// 	time.Sleep(2 * time.Second)
-// 	pin.Low()
-
-// 	return nil
-// }
-
 func TriggerRelay(pinNumber int) error {
-	fmt.Println("Mock trigger relay (Windows), pin:", pinNumber)
+	err := rpio.Open()
+	if err != nil {
+		return err
+	}
+	defer rpio.Close()
+
+	pin := rpio.Pin(pinNumber)
+	pin.Output()
+
+	pin.High()
+	time.Sleep(2 * time.Second)
+	pin.Low()
+
 	return nil
 }
+
+// func TriggerRelay(pinNumber int) error {
+// 	fmt.Println("Mock trigger relay (Windows), pin:", pinNumber)
+// 	return nil
+// }
