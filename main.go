f659950867c6fb17ecd99b7a8b3e50172bb42e72
alifnrz27 <alifnaufalrizki27@gmail.com>
2026-04-18 18:36:35 +0700

    edit

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"log"
+	"time"
 
 	"GateApp/backend/config"
 	"GateApp/backend/gpio"
 
 	"github.com/joho/godotenv"
+	"github.com/stianeikeland/go-rpio/v4"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
@@ -31,6 +34,41 @@ func main() {
 
 	// Start the application
 	config.Route(db)
+
+	// 🔥 Open GPIO
+	if err := rpio.Open(); err != nil {
+		fmt.Println("Failed to open GPIO:", err)
+		return
+	}
+	defer rpio.Close()
+
+	// 🔹 Setup pins (BCM mode default)
+	pin20 := rpio.Pin(20)
+	pin26 := rpio.Pin(26)
+	pin21 := rpio.Pin(21)
+
+	pin20.Output()
+	pin26.Output()
+	pin21.Output()
+
+	fmt.Println("Starting relay cycle...")
+
+	for {
+		// 🔹 Relay 21 ON
+		pin21.High()
+		time.Sleep(1 * time.Second)
+		pin21.Low()
+
+		// 🔹 Relay 20 ON
+		pin20.High()
+		time.Sleep(1 * time.Second)
+		pin20.Low()
+
+		// 🔹 Relay 26 ON
+		pin26.High()
+		time.Sleep(1 * time.Second)
+		pin26.Low()
+	}
 
 	err = gpio.Init()
 	if err != nil {
